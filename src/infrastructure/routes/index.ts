2c3f8323f84019ae480621a00a1b01f0da4c21b6
fraxcco <byfraxco@gmail.com>
2025-11-15 21:27:01 -0400

    added routes catch-all

--- src/infrastructure/routes/index.ts
+++ src/infrastructure/routes/index.ts
@@ -11,5 +11,11 @@ router.use(`${environment.API_VERSION}/auth`, authRoutes);
 router.use(`${environment.API_VERSION}/users`, userRoutes);
 router.use(`${environment.API_VERSION}/artifacts`, artifactRoutes);
 router.use(`${environment.API_VERSION}/characters`, characterRoutes);
+router.get(`${environment.API_VERSION}/`, (_req, res) => res.redirect("https://docs.teyvatapi.xyz/"));
+router.use((req, res, next) => {
+	if(req.path.startsWith(environment.API_VERSION)) return next();
+
+	return res.redirect("https://docs.teyvatapi.xyz/");
+});
 
 export default router;
